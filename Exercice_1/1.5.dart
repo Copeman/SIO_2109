15b0dc62d7a517101caded1cbaee3060be792d4e
Copeman <philip.copeman1@gmail.com>
2013-03-07 10:13:20 -0500

    initial

--- Exercice_1/1.5.dart
+++
@@ -1,5 +0,0 @@
-main(){
-  var sentence = "Écrivez un script qui compte le nombre d'occurrences du caractère « o » dans cette phrase.";
-  print(sentence.split("o").length-1);
-}
-
--- Exercice_1/1.9.dart
+++
@@ -1,7 +0,0 @@
-main() {
-  var liste = ['Jean-Michel', 'Marc', 'Vanessa', 'Anne', 'Maximilien', 'Alexandre-Benoît', 'Louise'];
-  for(var i = 0; i < liste.length; i++) {
-    print('${liste[i]} (${liste[i].length})');
-  }
-}
-
